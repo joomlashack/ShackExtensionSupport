3b4968813ae26dc9df57387166ab40dde8fc06cf
Anderson Grudtner Martins <anderson@ostraining.com>
2015-08-04 10:16:16 -0300

    Updates copyright information

--- src/include.php
+++ src/include.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package   OSMyLicensesManager
- * @contact   www.alledia.com, hello@alledia.com
- * @copyright 2014 Alledia.com, All rights reserved
+ * @contact   www.alledia.com, support@alledia.com
+ * @copyright 2015 Alledia.com, All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
