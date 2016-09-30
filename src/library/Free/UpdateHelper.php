f5e8d757b54464c592db9f48f7ba5522080e2290
Anderson Grudtner Martins <anderson@ostraining.com>
2016-09-29 22:30:10 -0300

    Removes unused method

--- src/library/Free/UpdateHelper.php
+++ src/library/Free/UpdateHelper.php
@@ -92,53 +92,6 @@ abstract class UpdateHelper
 
     /**
      * Get all Alledia Pro extensions and update the
-     * licence keys on the update url
-     *
-     * @param  string $keys
-     * @return void
-     */
-    public static function updateLicenseKeys($keys = '')
-    {
-        if (!empty($keys)) {
-            // Get the license keys, converting to a url param
-            $keys = base64_encode(preg_replace('/[^a-z0-9,]/i', '', $keys));
-
-            $allediaProExtensions = \Alledia\Framework\Helper::getAllediaExtensions('pro');
-            if (!empty($allediaProExtensions)) {
-                foreach ($allediaProExtensions as $extension) {
-                    $url = $extension->getUpdateURL();
-
-                    // Check if we already have any key on the update urls
-                    preg_match(
-                        '#^http[s]?://[a-z0-9\.]*/client/update/pro/[^/]+/[^/]*/?([a-z0-9=+]*)[/]?#i',
-                        $url,
-                        $matches
-                    );
-
-                    if (isset($matches[1])) {
-                        if ($matches[1] === $keys) {
-                            // We don't need to change this url
-                            continue;
-                        } else {
-                            // Remove the current key and the '/'
-                            $url = str_replace($matches[1], '', $url);
-                        }
-                    }
-
-                    // Add the key on the udpate url
-                    if (!preg_match('#/$#i', $url)) {
-                        $url .= '/';
-                    }
-                    $url .= $keys;
-
-                    $extension->setUpdateURL($url);
-                }
-            }
-        }
-    }
-
-    /**
-     * Get all Alledia Pro extensions and update the
      * release channel on the update url
      *
      * @param  string $channel
