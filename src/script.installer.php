e8ec98fc9c3d96710e9dc4810310409587ef3b9e
Anderson Grudtner Martins <anderson@ostraining.com>
2016-09-30 11:59:57 -0300

    Rebrands for Joomlashack

--- src/script.installer.php
+++ src/script.installer.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package   OSMyLicensesManager
- * @contact   www.alledia.com, support@alledia.com
- * @copyright 2015 Alledia.com, All rights reserved
+ * @contact   www.joomlashack.com, support@joomlashack.com
+ * @copyright 2015 Joomlashack.com, All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
