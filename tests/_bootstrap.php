fe3fc024cb7d58fd17f2026d221ee6718fe83d8a
Bill Tomczak <bill@joomlashack.com>
2019-05-14 11:32:08 -0700

    Updates for composer dependencies

--- tests/_bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// This is global bootstrap for autoloading
-define('BASE_PATH', realpath(__DIR__ . '/..'));
-define('VENDOR_PATH', realpath(BASE_PATH . '/vendor'));
-define('SRC_PATH', realpath(BASE_PATH . '/src'));
-define('TESTS_PATH', realpath(BASE_PATH . '/tests'));
-
-require_once  VENDOR_PATH . '/autoload.php';
